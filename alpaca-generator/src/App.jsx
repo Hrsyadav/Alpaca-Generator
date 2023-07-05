bae8932db1ec5290bd84a5ac5c229dbca9affc1a
Hrsyadav <30356923+Hrsyadav@users.noreply.github.com>
2023-07-05 10:06:20 +0530

    first commit \n file name change and folder structure creation

---
+++ alpaca-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from 'styled-components';
+
+function App(){
+    return(
+        <img src='/alpaca-generator/public/Media/accessories/earings.png'/>
+    );
+
+
+}
+
+export default App;
